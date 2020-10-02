19f7acab14c5b65677279ca1fc37e2b9071b426c
Matthew McCall <matthew@mccall.app>
2020-10-02 08:48:42 -0400

    Formatting updates

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App.js";
+import $ from 'jquery';
+
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/site.css'
 import { initializeIcons } from '@uifabric/icons';
-import $ from 'jquery';
+
 initializeIcons();
+
 $(function () {
     $('[data-toggle="tooltip"]').tooltip({ boundary: 'window' })
 })
+
 ReactDOM.render(<App />, document.getElementById("root"));
