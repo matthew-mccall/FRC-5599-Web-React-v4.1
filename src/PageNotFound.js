f930cd83be192d1b3373d6c4c154d88c85c36138
Matthew McCall <matthew@mccall.app>
2020-11-02 10:01:18 -0500

    Added 404 page

---
+++ src/PageNotFound.js
@@ -0,0 +1,40 @@
+import * as React from 'react';
+import * as FeatherIcon from 'react-feather';
+import { hot } from "react-hot-loader";
+import { NeutralColors } from '@fluentui/theme';
+import { Text } from "@fluentui/react/lib/Text"
+import $ from 'jquery';
+
+
+class PageNotFound extends React.Component {
+
+    componentDidMount() {
+        $(".parallax").each(function () {
+            $(this).css("background", $(this).attr("data-bg"))
+            $(this).css("background-size", "cover")
+            $(this).css("background-position", "center center")
+            if ($(window).width() >= 768) {
+                $(this).css("background-attachment", "fixed")
+            } else {
+                $(this).css("background-attachment", "scroll")
+            }
+        })
+    }
+
+    render() {
+        return (
+            <div className="row flex-grow-1" style={{ color: NeutralColors.gray30 }}>
+                <div className="col my-auto">
+                    <div className="container py-5">
+                        <main>
+                            <h1>404 - Page Not Found</h1>
+                            <p>We're sorry, the page you requested does not exist.</p>
+                        </main>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default hot(module)(PageNotFound);
